59813ae73be522dd11bcedb79f5bd8f013a4903f
Kryštof Novosad <novosadkry@seznam.cz>
2024-01-06 21:40:41 +0100

    Scrapbook query

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,25 @@
 use std::env;
-use dotenv::dotenv;
-use anyhow::{Context, Result, bail};
-use sf_api::session::{CharacterSession, ServerConnection};
+use anyhow::{Context, Result};
 
+#[derive(Debug)]
 pub struct Config {
-    login: String,
-    password: String,
-    server_url: String
+    pub login: String,
+    pub password: String
 }
 
 impl Config {
     pub fn from_env() -> Result<Self> {
-        let _ = dotenv();
-
         Ok(Self {
             login: env::var("SFGAME_USERNAME").context("SFGAME_USERNAME")?,
-            password: env::var("SFGAME_PASSWORD").context("SFGAME_PASSWORD")?,
-            server_url: env::var("SFGAME_SERVER_URL").context("SFGAME_SERVER_URL")?
+            password: env::var("SFGAME_PASSWORD").context("SFGAME_PASSWORD")?
         })
     }
 }
 
-pub trait CharacterSessionExt {
-    fn from_config(config: Config) -> Result<CharacterSession>;
-}
+pub fn init() -> Result<()> {
+    dotenv::dotenv()?;
 
-impl CharacterSessionExt for CharacterSession {
-    fn from_config(config: Config) -> Result<Self> {
-        Ok(CharacterSession::new(
-            &config.login,
-            &config.password,
-            match ServerConnection::new(&config.server_url) {
-                Some(connection) => connection,
-                None => bail!("Invalid server URL!"),
-            },
-        ))
-    }
+    let env = env_logger::Env::default().default_filter_or("info");
+    env_logger::Builder::from_env(env).try_init()
+        .context("Failed to initialize logger!")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,93 @@
+use std::{io, collections::HashSet};
+use log::{info, debug};
 use anyhow::{Context, Result};
+use tokio::time::{sleep, Duration};
 use sf_api::{
+    sso::SFAccount,
     session::CharacterSession,
     gamestate::GameState,
     command::Command
 };
-use tokio::time::{sleep, Duration};
 
-use sfscraper::{Config, CharacterSessionExt};
+use sfscraper::Config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    sfscraper::init()?;
+
     let config = Config::from_env()
         .context("Invalid or missing configuration")?;
 
-    let mut session = CharacterSession::from_config(config)?;
+    let account = SFAccount::login(config.login, config.password).await?;
+    let mut sessions: Vec<CharacterSession> = account.characters().await?
+        .into_iter().flatten()
+        .collect();
+
+    println!("Please enter desired character number:");
+    for (i, session) in sessions.iter().enumerate()
+    {
+        println!("[{}] {} ({})", i, session.username(), session.server_url())
+    }
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+
+    let index = input.trim().parse::<usize>()?;
+    let session = sessions
+        .get_mut(index)
+        .context("Invalid selection")?;
 
     let login_response = session.login().await?;
     let mut game_state = GameState::new(login_response)?;
 
-    println!("Logged in as {}", game_state.character.name);
+    info!("Logged in as {} ({})", session.username(), session.server_url());
 
-    println!("Sending player update");
+    info!("Sending player update");
     let response = session.send_command(&Command::UpdatePlayer).await?;
     game_state.update(response)?;
 
-    println!("Waiting a few milliseconds");
+    info!("Waiting a few milliseconds");
     sleep(Duration::from_millis(250)).await;
 
-    println!("Getting scrapbook contents");
+    info!("Getting scrapbook contents");
     let response = session.send_command(&Command::ViewScrapbook).await?;
     game_state.update(response)?;
 
     let scrapbook = game_state.unlocks.scrapbok
+        .clone()
         .context("Your character doesn't have an active scrapbook!")?;
 
-    println!("{:?}", scrapbook.items);
+    debug!("{:?}", scrapbook.items);
+
+    info!("Waiting a few milliseconds");
+    sleep(Duration::from_millis(250)).await;
+
+    info!("Getting players from hall of fame");
+    let response = session.send_command(&Command::HallOfFamePage { page: 650 }).await?;
+    game_state.update(response)?;
+
+    let mut players_to_attack = HashSet::new();
+    let hall_entries = game_state.other_players.hall_of_fame.clone();
+
+    for hall_entry in hall_entries.iter() {
+        info!("Waiting a few milliseconds");
+        sleep(Duration::from_millis(1000)).await;
+
+        info!("Viewing player {} details", hall_entry.name);
+        let response = session.send_command(&Command::ViewPlayer { ident: hall_entry.name.clone() }).await?;
+        game_state.update(response)?;
+
+        if let Some(player) = game_state.other_players.lookup_name(&hall_entry.name) {
+            for equip in player.equipment.0.iter().flatten() {
+                let equip_ident = equip.equipment_ident().unwrap();
+                if !scrapbook.items.contains(&equip_ident) {
+                    debug!("Player {} has an item you haven't discovered yet", player.name);
+                    players_to_attack.insert(player.name.clone());
+                    break;
+                }
+            }
+        }
+    }
 
     Ok(())
 }
