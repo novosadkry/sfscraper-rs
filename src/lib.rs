ab3ed43ed50e658c61bc6aa07211b6de0a9ef675
Kryštof Novosad <novosadkry@seznam.cz>
2024-01-08 00:17:33 +0100

    this is bad

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,5 @@
-use std::{
-    env,
-    sync::Arc,
-    collections::HashSet
-};
-use log::{info, debug};
+use std::env;
 use anyhow::{Context, Result};
-use tokio::{
-    sync::Mutex,
-    time::{sleep, Duration},
-    join
-};
-use sf_api::{
-    gamestate::{
-        GameState,
-        unlockables::ScrapBook
-    },
-    command::Command,
-    session::CharacterSession
-};
 
 #[derive(Debug)]
 pub struct Config {
@@ -32,68 +14,4 @@ impl Config {
             password: env::var("SFGAME_PASSWORD").context("SFGAME_PASSWORD")?
         })
     }
-}
-
-pub async fn thread_player_update() {
-
-}
-
-pub async fn thread_player_attack() {
-
-}
-
-pub async fn thread_scrape_halloffame(session: Arc<Mutex<CharacterSession>>, game_state: Arc<Mutex<GameState>>) -> Result<()> {
-    loop {
-        let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
-
-        info!("Waiting a few milliseconds");
-        sleep(Duration::from_millis(250)).await;
-
-        info!("Getting scrapbook contents");
-        let response = session.send_command(&Command::ViewScrapbook).await?;
-        game_state.update(response)?;
-
-        let scrapbook = game_state.unlocks.scrapbok
-            .clone()
-            .context("Your character doesn't have an active scrapbook!")?;
-
-        debug!("{:?}", scrapbook.items);
-
-        info!("Waiting a few milliseconds");
-        sleep(Duration::from_millis(250)).await;
-
-        info!("Getting players from hall of fame");
-        let response = session.send_command(&Command::HallOfFamePage { page: 650 }).await?;
-        game_state.update(response)?;
-
-        let players_to_attack = get_players_to_attack(&mut session, &mut game_state, &scrapbook).await?;
-        debug!("{:?}", players_to_attack);
-    }
-}
-
-pub async fn get_players_to_attack(session: &mut CharacterSession, game_state: &mut GameState, scrapbook: &ScrapBook) -> Result<HashSet<String>> {
-    let mut players_to_attack = HashSet::new();
-    let hall_entries = game_state.other_players.hall_of_fame.clone();
-
-    for hall_entry in hall_entries.iter() {
-        info!("Waiting a few milliseconds");
-        sleep(Duration::from_millis(1000)).await;
-
-        info!("Viewing player {} details", hall_entry.name);
-        let response = session.send_command(&Command::ViewPlayer { ident: hall_entry.name.clone() }).await?;
-        game_state.update(response)?;
-
-        if let Some(player) = game_state.other_players.lookup_name(&hall_entry.name) {
-            for equip in player.equipment.0.iter().flatten() {
-                let equip_ident = equip.equipment_ident().unwrap();
-                if !scrapbook.items.contains(&equip_ident) {
-                    debug!("Player {} has an item you haven't discovered yet", player.name);
-                    players_to_attack.insert(player.name.clone());
-                    break;
-                }
-            }
-        }
-    }
-
-    Ok(players_to_attack)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use std::{io, sync::Arc};
-use log::{info, debug};
+use std::{
+    io,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering}
+    }
+};
+use chrono::Local;
+use log::{info, debug, warn};
 use anyhow::{Context, Result, bail};
-use tokio::{signal, sync::Mutex};
+use tokio::{
+    signal,
+    time::{sleep, Duration},
+    sync::Mutex,
+    join
+};
 use sf_api::{
     sso::SFAccount,
     session::CharacterSession,
     gamestate::GameState,
-    command::Command
+    command::Command,
 };
 
 use sfscraper::Config;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let running = Arc::new(AtomicBool::new(true));
     dotenv::dotenv()?;
 
     let env = env_logger::Env::default().default_filter_or("info");
@@ -44,20 +57,167 @@ async fn main() -> Result<()> {
     };
 
     let login_response = session.login().await?;
-    let mut game_state = GameState::new(login_response)?;
+    let game_state = GameState::new(login_response)?;
 
-    info!("Logged in as {} ({})", session.username(), session.server_url());
-
-    info!("Sending player update");
-    let response = session.send_command(&Command::UpdatePlayer).await?;
-    game_state.update(response)?;
+    info!(target: "main", "Logged in as {} ({})", session.username(), session.server_url());
 
     let session = Arc::new(Mutex::new(session));
     let game_state = Arc::new(Mutex::new(game_state));
 
-    let scrape_handle = tokio::spawn(sfscraper::thread_scrape_halloffame(session.clone(), game_state.clone()));
+    let signal_task = tokio::spawn(signal_task_entry(running.clone()));
+    let player_update_task = tokio::spawn(player_update_task_entry(running.clone(), session.clone(), game_state.clone()));
+    let search_and_attack_task = tokio::spawn(search_and_attack_entry(running.clone(), session.clone(), game_state.clone()));
 
+    let results = join!(signal_task, player_update_task, search_and_attack_task);
+    results.0??; results.1??; results.2??;
+
+    Ok(())
+}
+
+pub async fn command(
+    session: &mut CharacterSession,
+    game_state: &mut GameState,
+    command: &Command) -> Result<()>
+{
+    let mut retries = 0;
+
+    while retries < 5 {
+        let response = session.send_command(command).await?;
+        sleep(Duration::from_millis(250)).await;
+
+        if let Err(_) = game_state.update(response) {
+            warn!(target: "main", "Server error, attempting reconnect");
+
+            let response = session.login().await?;
+            sleep(Duration::from_secs(1)).await;
+
+            game_state.update(response)?;
+        } else {
+            return Ok(());
+        }
+
+        retries += 1;
+    }
+
+    bail!("Maximum number of retries reached")
+}
+
+pub async fn signal_task_entry(
+    running: Arc<AtomicBool>) -> Result<()>
+{
     signal::ctrl_c().await?;
+    running.store(false, Ordering::SeqCst);
+
+    Ok(())
+}
+
+pub async fn player_update_task_entry(
+    running: Arc<AtomicBool>,
+    session: Arc<Mutex<CharacterSession>>,
+    game_state: Arc<Mutex<GameState>>) -> Result<()>
+{
+    while running.load(Ordering::SeqCst) {
+        {
+            let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+            debug!(target: "update", "Sending player update");
+            command(&mut session, &mut game_state, &Command::UpdatePlayer).await?;
+        }
+
+        debug!(target: "update", "Waiting 5 minutes before next update");
+        sleep(Duration::from_secs(300)).await;
+    }
+
+    Ok(())
+}
+
+pub async fn search_and_attack_entry(
+    running: Arc<AtomicBool>,
+    session: Arc<Mutex<CharacterSession>>,
+    game_state: Arc<Mutex<GameState>>) -> Result<()>
+{
+    let mut page = 20_000 / 30;
+
+    let mut scrapbook = {
+        let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+        info!(target: "search", "Updating scrapbook contents");
+        command(&mut session, &mut game_state, &Command::ViewScrapbook).await?;
+
+        game_state.unlocks.scrapbok
+            .clone()
+            .context("Your character doesn't have an active scrapbook!")?
+    };
+
+    while running.load(Ordering::SeqCst) {
+        let hall_entries = {
+            let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+            info!(target: "search", "Getting players from hall of fame (index: {})", page * 30);
+            command(&mut session, &mut game_state, &Command::HallOfFamePage { page }).await?;
+
+            game_state.other_players.hall_of_fame.clone()
+        };
+
+        for hall_entry in hall_entries.iter() {
+            let player = {
+                let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+                debug!(target: "search", "Viewing player {} details", hall_entry.name);
+                command(&mut session, &mut game_state, &Command::ViewPlayer { ident: hall_entry.name.clone() }).await?;
+
+                game_state.other_players.lookup_name(&hall_entry.name)
+                    .context("Player lookup failed")?
+                    .clone()
+            };
+
+            for equip in player.equipment.0.iter().flatten() {
+                let equip_ident = equip.equipment_ident().unwrap();
+                if !scrapbook.items.contains(&equip_ident) {
+                    info!(target: "search", "Player {} has an item you haven't discovered yet", player.name);
+
+                    loop {
+                        let free_fight = {
+                            let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+                            info!(target: "search", "Checking if free fight is available");
+                            command(&mut session, &mut game_state, &Command::CheckArena).await?;
+
+                            game_state.arena.next_free_fight.context("Free fight unavailable")?
+                        };
+
+                        let wait_time = free_fight.time() - Local::now().time();
+
+                        if wait_time.num_milliseconds() > 0 {
+                            let wait_time = wait_time + chrono::Duration::seconds(5);
+                            info!(target: "search", "Waiting {} seconds until free fight is available", wait_time.num_seconds());
+                            sleep(Duration::from_millis(u64::try_from(wait_time.num_milliseconds()).unwrap())).await;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    {
+                        let (mut session, mut game_state) = join!(session.lock(), game_state.lock());
+
+                        info!(target: "search", "Fighting player {}", player.name);
+                        command(&mut session, &mut game_state, &Command::Fight { name: player.name.clone(), use_mushroom: false }).await?;
+
+                        info!(target: "search", "Updating scrapbook contents");
+                        command(&mut session, &mut game_state, &Command::ViewScrapbook).await?;
+
+                        scrapbook = game_state.unlocks.scrapbok
+                            .clone()
+                            .context("Your character doesn't have an active scrapbook!")?;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        page -= 1;
+    }
 
     Ok(())
 }
